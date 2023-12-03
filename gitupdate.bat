e51b29464e1c93ad4276412bd63d3764ab4a166c
Filip Mojto <fmojto@gmail.com>
2023-12-03 16:23:31 +0100

    Added new functionality

--- gitupdate.bat
+++ gitupdate.bat
@@ -1,12 +1,12 @@
 @echo off
 
-if "%1"=="" (
-    echo Usage: %0 [update_label] [branch]
+if "%~1"=="" (
+    echo Usage: %0 "update_label" [branch]
     exit /b 1
 )
 
 git add .
-git commit -m "%1"
+git commit -m "%~1"
 
 if "%2"=="" (
     git push origin main
