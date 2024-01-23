ed774b8116967180cffe986392c144c366cea2d6
Filip Mojto <fmojto@gmail.com>
2024-01-23 16:51:24 +0100

    categories file included

--- code/matching.py
+++ code/matching.py
@@ -4,47 +4,46 @@ from typing import List, Dict
 
 class ProductMatcher:
 
-    def __init__(self, product_file: str) -> None:
+    def __init__(self, product_file: str, adjust_at: int = 1000) -> None:
         self.__product_file = product_file
-
-    def __top_matches(self, matches: List[Dict[Product, float]], limit: int):
-        matches.sort(key=lambda x: list(x.values()), reverse=True)
-
-        if len(matches) <= limit:
-            return matches
-        else:
-            return matches[0 : limit + 1]
-
-
+        self.__adjust_at = adjust_at
+        self.__matches: List[tuple[Product, float]] = []
     
-    def match(self, text: str, headerless: bool = True, limit: int = 5):
-        with open(file=self.__product_file, mode='r', encoding='utf-8') as file:
-            
-            top_matches: List[Dict[Product, float]] = []
+    def adjust_at(self, matches: int = None):
+        if matches is not None:
+            self.__adjust_at = matches
+        
+        return self.adjust_at
 
+    def match(self, text: str, category: str = None, headerless: bool = True, limit: int = 5):
+
+        if limit >= self.__adjust_at:
+            raise ValueError("Matches limit must be lower than adjustment point.")
+
+        with open(file=self.__product_file, mode='r', encoding='utf-8') as file:
+            self.__matches.clear()
 
             line = file.readline()
 
             if not headerless:
                 line = file.readline()
-
-
         
             while(line):
-                #attributes = line.split(sep=Market.PF_ATTR_DELIMITER)
-                
                 product = Product.to_product(row=line)
-
-
-                top_matches.append({ product : fuzz.partial_ratio(product.name().lower(), text.lower()) })
+                self.__matches.append( (product, fuzz.partial_ratio(product.name().lower(), text.lower())) )
                 
-                if len(top_matches) > 1000:
-                    top_matches = self.__top_matches(matches=top_matches, limit=limit)
+                if len(self.__matches) > self.__adjust_at:
+                    self.__adjust_matches(category=category, limit=limit)
 
-                # print(f"{row[Market.PF_NAME_INDEX]}: {fuzz.token_set_ratio(line.split(Market.PF_ATTR_DELIMITER)[Market.PF_NAME_INDEX], text)}")
-                # line = file.readline()
                     
                 line = file.readline()
             
-        return self.__top_matches(matches=top_matches, limit=limit)
+        self.__adjust_matches(category=category, limit=limit)
+        return self.__matches
 
+    def __adjust_matches(self, category: str, limit: int) -> bool:
+        self.__matches.sort(key=lambda pair: pair[1], reverse=True)
+        
+        filter(lambda pair: pair[0].category() == category,  self.__matches)
+        
+        self.__matches = self.__matches[:limit]
--- code/test.py
+++ code/test.py
@@ -1,48 +1,9 @@
-from tkinter import Tk, Menu
+from matching import ProductMatcher
+import config_paths
 
-def file_new():
-    print("New File")
+matcher = ProductMatcher(product_file=config_paths.PRODUCT_FILE_PATH)
 
-def file_open():
-    print("Open File")
+products = matcher.match(text="Energetak", headerless=False, limit=10, category=)
 
-def file_save():
-    print("Save File")
-
-# def edit_cut():
-#     print("Cut")
-
-# def edit_copy():
-#     print("Copy")
-
-# def edit_paste():
-#     print("Paste")
-
-# Create the main application window
-app = Tk()
-app.title("Menu Example")
-
-# Create a Menu widget
-main_menu = Menu(app)
-app.config(menu=main_menu)
-
-# Create a File menu
-file_menu = Menu(main_menu, tearoff=0)
-main_menu.add_cascade(label="File", menu=file_menu)
-
-file_menu.add_command(label="New", command=file_new)
-file_menu.add_command(label="Open", command=file_open)
-file_menu.add_command(label="Save", command=file_save)
-file_menu.add_separator()
-file_menu.add_command(label="Exit", command=app.quit)
-
-# # Create an Edit menu
-# edit_menu = Menu(main_menu, tearoff=0)
-# main_menu.add_cascade(label="Edit", menu=edit_menu)
-
-# edit_menu.add_command(label="Cut", command=edit_cut)
-# edit_menu.add_command(label="Copy", command=edit_copy)
-# edit_menu.add_command(label="Paste", command=edit_paste)
-
-# Start the Tkinter event loop
-app.mainloop()
+for product in products:
+    product[0].__str__()
